12caeea42a32f54d59860e6c3dccfca2466c08b1
rogerioneo <rogerio.neo@gmail.com>
2019-09-06 20:52:30 -0300

    Lista inteiros populando de forma diferente (step)

--- app/src/main/java/br/edu/ifsp/scl/trykotlinsdm/colecoes.kt
+++ app/src/main/java/br/edu/ifsp/scl/trykotlinsdm/colecoes.kt
@@ -10,7 +10,8 @@ fun main(args: Array<String>) {
     }
 
     // Lista de inteiros
-    val listaInteiros: List<Int> = listOf(1,2,3,4,5,6,7,8,9,10,11,12,13)
+//    val listaInteiros: List<Int> = listOf(1,2,3,4,5,6,7,8,9,10,11,12,13)
+    val listaInteiros: List<Int> = (1..10).toList()
 
 //    listaInteiros.add(11) //Erro - Lista imutável
 //    listaInteiros = listOf() //Erro - referência é imutável
@@ -18,5 +19,10 @@ fun main(args: Array<String>) {
     //Percorrer com for usando IntRange
     for (i in 0..9){
         println(listaInteiros[i])
+    }
+
+    val listaInteiros2: List<Int> = (1..10 step 2).toList()
+    for (lista in listaInteiros2){
+        println(lista)
     }
 }
