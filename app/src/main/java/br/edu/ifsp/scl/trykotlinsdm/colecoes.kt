1340fd82f0d44b9a3b6f570e1139b092d19dcbde
rogerioneo <rogerio.neo@gmail.com>
2019-09-06 20:48:43 -0300

    Exemplo de lista imutável Inteiros

--- app/src/main/java/br/edu/ifsp/scl/trykotlinsdm/colecoes.kt
+++ app/src/main/java/br/edu/ifsp/scl/trykotlinsdm/colecoes.kt
@@ -8,4 +8,15 @@ fun main(args: Array<String>) {
     for (integrante in familia){
         println(integrante)
     }
+
+    // Lista de inteiros
+    val listaInteiros: List<Int> = listOf(1,2,3,4,5,6,7,8,9,10,11,12,13)
+
+//    listaInteiros.add(11) //Erro - Lista imutável
+//    listaInteiros = listOf() //Erro - referência é imutável
+
+    //Percorrer com for usando IntRange
+    for (i in 0..9){
+        println(listaInteiros[i])
+    }
 }
